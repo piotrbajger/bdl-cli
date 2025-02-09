2d2ef765a4cd5f9673fa831408a5573d946accbe
Piotr Bajger <piotr.bajger@hotmail.com>
2025-02-09 20:51:42 +0100

    Move configuration to external files

--- get_data.py
+++ get_data.py
@@ -5,10 +5,6 @@ import requests
 import pandas as pd
 
 
-VARIABLE_IDS = ["744856", "744857"]
-UNIT_IDS = ["012415075011"]
-
-
 MAX_VAR_NAME_LEVEL = 10
 BASE_URL = "https://bdl.stat.gov.pl/api/v1"
 HEADERS = {
@@ -51,7 +47,7 @@ def get_variables():
         return parsed_dict
 
     variables = []
-    for var_id in VARIABLE_IDS:
+    for var_id in get_variable_ids():
         url = f"{BASE_URL}/variables/{var_id}"
         params = {"format": "json", "lang": "pl"}
         var_json = _get_json_response(url, params)
@@ -83,16 +79,34 @@ def json_to_dataframe(json_data):
     return pd.DataFrame(records)
 
 
+def get_unit_ids():
+    with open("jednostki.txt") as f:
+        unit_ids = f.readlines()
+    unit_ids = [u.strip() for u in unit_ids if u]
+    print(f"Ściągam dane dla {len(unit_ids)} jednostek terytorialnych.")
+    return unit_ids
+
+
+def get_variable_ids():
+    with open("zmienne.txt") as f:
+        var_ids = f.readlines()
+    var_ids = [v.strip() for v in var_ids if v]
+    print(f"Ściągam dane dla {len(var_ids)} zmiennych.")
+    return var_ids
+
+
 if __name__ == "__main__":
+    unit_ids = get_unit_ids()
     variables = get_variables()
     variable_ids = variables["variable_id"].to_list()
     all_data = []
-    for unit_id in UNIT_IDS:
+    for unit_id in unit_ids:
         unit_data = get_unit_data(
             unit_id="012415075011",
             variable_ids=variable_ids,
         )
 
+    output_path = "data/all_data.csv"
     df = unit_data.merge(variables, how="left", on="variable_id")
     df_wide = df.pivot(
         index=["unit_id", "variable_id", "unit_name", "variable_name", "variable_unit"],
@@ -100,5 +114,6 @@ if __name__ == "__main__":
         values="value",
     )
     df_wide = df_wide.reset_index()
-    df_wide.to_csv("data/all_data.csv", index=False, encoding="utf-8")
-    print(df_wide)
+    df_wide.to_csv(output_path, index=False, encoding="utf-8")
+
+    print(f"Zapisuję {len(df_wide)} wierszy w {output_path}.")
